3316f25ed3db29464f5f7a9f5be6a821ebd0db6c
Tshakalisa <tkhupe@gmail.com>
2023-02-26 06:40:06 -0600

    added function

---
+++ log-even-nums.js
@@ -0,0 +1,13 @@
+// Write code to print all even numbers from 0 to `num`
+// Assume `num` will be a positive number
+
+var logEvenNums = function(num) {
+    var even = []
+    for (let i = 0; i < num; i++) {
+        if (i % 2 === 0) {
+            even.push(i)
+
+        }
+        console.log(even);
+    }
+};
